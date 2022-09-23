eb1d66c21ca57447c569e0791622c72211a8b081
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2022-09-23 09:14:29 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class uczen{
+public:
+	string imie;
+	string nazwisko;
+	int wiek;
+	
+	uczen(string imie, string nazwisko, int wiek) {
+		this->imie = imie;
+		this->nazwisko = nazwisko;
+		this->wiek = wiek;
+	}
+
+	uczen(string imie, string nazwisko) {
+		this->imie = imie;
+		this->nazwisko = nazwisko;
+	}
+
+	uczen() {
+		this->imie = "brak";
+		this->nazwisko ="brak";
+		this->wiek = 0;
+	}
+
+	void wyswietl(void) {
+		cout << "Imie: " << this->imie<<endl;
+
+	}
+	
+};
+
+
+int main() {
+	uczen* u1 = new uczen("Jan", "Kowalski", 12);
+	uczen* u2 = new uczen("Adam", "Nowak");
+	uczen* u3 = new uczen();
+	u1->wyswietl();
+}
